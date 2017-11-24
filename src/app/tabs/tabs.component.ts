56ab6acad63a0d4f1d12460f104ada489ad7ee28
Younghoon Jo (Eddy) <edwardevjo@gmail.com>
2017-11-24 00:22:20 -0500

    version3 fix design

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class TabsComponent {  
+
 }
